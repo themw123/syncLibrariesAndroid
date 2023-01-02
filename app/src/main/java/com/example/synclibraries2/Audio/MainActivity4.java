e7ea6672bfec42165b7b2932b65046e0c5596901
themw123 <80266862+themw123@users.noreply.github.com>
2023-01-02 22:36:31 +0100

    n

--- app/src/main/java/com/example/synclibraries2/Audio/MainActivity4.java
+++ app/src/main/java/com/example/synclibraries2/Audio/MainActivity4.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import com.example.synclibraries2.R;
 
+import syncLibraries.Audio;
 import syncLibraries.SocketClient;
 
 public class MainActivity4 extends AppCompatActivity{
@@ -19,32 +20,37 @@ public class MainActivity4 extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        client = new SocketClient("192.168.0.138", 9876);
-        client.openSocket();
+        openWebsite();
+        //closeBrowser();
+    }
 
+    private void openWebsite() {
         Thread t = new Thread(() -> {
-            try {
-                Thread.sleep(5000);
-                client.writeToServer("https://bs.to/serie/Westworld/3/1-Parce-Domine/de/Streamtape");
-                Thread.sleep(5000);
-                //client.closeSocket();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            Audio audio = new Audio("192.168.0.67", 32400, "fYyu_nN9V9_jqpZ6K6UF");
+            url = audio.getBsUrl();
+            if(url == null) {
+                return;
             }
-
+            //erst per ssh server socket starten dann weiter
+            client = new SocketClient("192.168.0.138", 9876);
+            client.writeToServer(url);
         });
         t.start();
 
     }
 
+
+    private void closeBrowser() {
+        client.closeSocket();
+        //auch noch serversocket script auf pc schließen (kein ssh nötig weil script mit writeToServer("exit"); beendet wird durch client)
+    }
+
+    /*
     @Override
     protected void onStop() {
         super.onStop();
-        // Perform some action here
-
         client.closeSocket();
-
     }
-
+    */
 
 }
